1e255b5f374fd7b6bf848202003cc8816b4da40b
Silvio <nirvash533@gmail.com>
2024-05-21 18:46:52 -0500

    update

--- index.js
+++ index.js
@@ -6,9 +6,20 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
 
-app.post("/submit", (req, res) => {});
+res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  //on this line we sum the number of leter the user input submitting his/her name
+const numberOfLetters = req.body["fName"].length + req.body["lName"].length; 
+console.log(numberOfLetters);
+
+res.render("index.ejs", {
+  nLetters: numberOfLetters 
+});
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
